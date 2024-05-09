542abaddf7615efd86e15b73fcf519adff85442b
Haikal R Fadhilah <haikal.fadhilah13@gmail.com>
2024-05-10 01:42:44 +0700

    fix: no animation navbar mobile

--- js/script.js
+++ js/script.js
@@ -2,10 +2,14 @@ const handlerMenuList = () => {
   const navbar = document.getElementById("menu-list");
   console.log(navbar.classList.contains("translate-x-full"));
   if (navbar.classList.contains("translate-x-full")) {
+    navbar.classList.remove("hidden");
+    navbar.classList.add("flex");
     navbar.classList.remove("translate-x-full");
     navbar.classList.add("translate-x-0");
   } else {
     navbar.classList.remove("translate-x-0");
     navbar.classList.add("translate-x-full");
+    navbar.classList.remove("flex");
+    navbar.classList.add("hidden");
   }
 };
